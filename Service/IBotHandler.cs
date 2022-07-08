69d5fe642c0fd5ea7e498f7ed7a7515109097881
dinhnt <compimprove@gmail.com>
2022-07-08 14:42:04 +0700

    up

---
+++ Service/IBotHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using ChessGame.Models;
+using ChessGame.Models.Chess;
+
+namespace ChessGame.Service
+{
+    public interface IBotHandler
+    {
+        public Task HandleMove(GameBoard gameBoard, Board board);
+    }
+}
